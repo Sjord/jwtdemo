00d734de09022a4caf46999706e61b09331a26ab
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2016-09-19 13:58:22 +0200

    Simplified Firebase example

--- firebasejwt.php
+++ firebasejwt.php
@@ -1,58 +1,28 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/common.php';
+
 use \Firebase\JWT\JWT;
 
 JWT::$leeway = 10; // $leeway in seconds
 
-$shared_key = "secret";
-$private_key = file_get_contents('private.pem');
-$public_key = file_get_contents('public.pem');
-
-$auth_header = $_SERVER['HTTP_AUTHORIZATION'];
-if (stripos($auth_header, 'bearer ') == 0) {
-    $auth_header = substr($auth_header, 7);
-}
-
-JWT::$supported_algs['none'] = ['hash_hmac', 'none'];
-$algorithms = [
-    'none' => 'none',
-    'HS256' => $shared_key,
-    'RS256' => $public_key,
-];
-
-if ($auth_header) {
-    $header = json_decode(base64_decode(substr($auth_header, 0, strpos($auth_header, '.'))));
-    $key = $algorithms[$header->alg];
-    try {
-        $decoded = JWT::decode($auth_header, $key, array_keys($algorithms));
-        echo "Valid $algorithm JWT: ";
-        print_r($decoded);
-    } catch (Exception $e) {
-        echo "Invalid JWT: $e\n";
+function encodeJwt($tokenObj, $algorithmName, $keys) {
+    if ($algorithmName == 'none') {
+        return "not supported";
     }
-} else {
-    $token = array(
-        # Issuer
-        "iss" => "http://demo.sjoerdlangkemper.nl/",
 
-        # Issued at
-        "iat" => time(),
-
-        # Expire
-        "exp" => time() + 120,
-
-        "data" => [
-            "hello" => "world"
-        ]
-    );
-
-    echo '<xmp>';
-
-    $jwt = JWT::encode($token, $shared_key, 'HS256');
-    echo "HS256: $jwt\n";
-
-    $jwt = JWT::encode($token, $private_key, 'RS256');
-    echo "RS256: $jwt\n";
-
-    echo "Public key: \n$public_key\n";
+    $key = $keys;
+    if (is_array($keys)) {
+        $key = $keys[0];
+    }
+    return JWT::encode($tokenObj, $key, $algorithmName);
 }
+
+function decodeJwt($token) {
+    $keys = getAlgorithmKeys();
+    $key = $keys['RS256'][1];
+
+    return JWT::decode($token, $key, array_keys($keys));
+}
+
+include('base.php');
